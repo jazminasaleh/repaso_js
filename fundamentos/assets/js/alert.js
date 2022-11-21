e9feeb0e7a04920a8b109043d66cb50c9b1549a0
jazmin saleh peña <jazminasaleh@gmail.com>
2022-11-21 10:48:59 -0500

    primer commit

---
+++ fundamentos/assets/js/alert.js
@@ -0,0 +1,8 @@
+//*Esto solo sirve para ejectuar en el navegador web
+alert('Hola mundo');
+
+let nombre = prompt('Cual es tu nombre', 'No tienbe nombre');
+console.log(nombre);
+
+const seleccion = confirm('Estas seguro de borrar esto?');
+console.log(seleccion);
---
+++ fundamentos/assets/js/app.js
@@ -0,0 +1,20 @@
+//Comentarios
+//* Primer comentraio
+//? segundo comentario 
+
+
+//*Tipos de variables
+let a = 10, d=0, x = a+d;
+var b = 15;
+const c = 20;
+
+a = 80; 
+
+
+//?Console
+console.log('Hola mundo en js');
+console.error('este es c '+c+ ' este es d ' + d);
+console.warn('este es a ',a);
+console.log({a});//*{a : 80}
+console.log({b});
+console.table({a,d,x,b,c});//*sale tabla con nom y valor
