b8fe93164f37e5b33c6cb4d21f309c68f3c38a51
jazmin saleh peña <jazminasaleh@gmail.com>
2022-11-22 09:08:51 -0500

    palabras reservadas

---
+++ fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,9 @@
+//*Nombre de varaibles que no comience con numeros
+//* puede tener $
+//*No tenga puntos el nombre de la variable
+//*Si es atributo se coloca jugandoConPuntajes primera minusucula
+//*Si es clase o metodo JugandoConPuntaje primera mayuscula
+var π = Math.PI;
+var ಠ_ಠ = eval;
+var λ = function() {};
+var Ꙭൽↈⴱ = 'huh';
